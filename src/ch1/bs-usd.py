7e730c13eb562ecf0b10b1ea4e70d0ceebe1f650
yiseungju <kkan79@gmail.com>
2018-11-06 23:22:42 +0900

    html 출력

--- src/ch1/bs-usd.py
+++ src/ch1/bs-usd.py
@@ -8,12 +8,17 @@ soup = BeautifulSoup(reponse, "html.parser")
 results = soup.select("ul.data_lst > li")
 
 for result in results:
+    print(result)
+
+
+for result in results:
     nation = result.select_one("h3 > span.blind")
     value = result.select_one("span.value")
     change_num = result.select_one("span.change")
     change_txt = result.select_one("div > span.blind")
 
     print(nation.string, value.string + "원", change_num.string, change_txt.string)
+
 
 
 
